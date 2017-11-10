78425421cb1e243924e27016a2e7dfa28cd6a1f1
harshthakur07 <31073587+harshthakur07@users.noreply.github.com>
2017-11-10 13:18:39 +0530

    Add files via upload

---
+++ include/db.php
@@ -0,0 +1,8 @@
+<?php
+	$server="localhost";
+	$username="root";
+	$password="";
+	$database="harshdb";
+	
+	$conn=mysqli_connect($server,$username,$password,$database);
+?>
---
+++ include/header.php
@@ -0,0 +1,3 @@
+<div class="header text-center" style="">
+	<a href="index.php"><h3>Hindi Movies Database</h3></a>
+</div>
